f0e491d71a2c8b994fe0af20f72d367c967c4803
2k0ri <esc13245@gmail.com>
2017-01-06 01:39:47 +0900

    add link, tweak upload.sh

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # rancheros-packer-vmware-cloudstack
 
 - RancherOS image for CloudStack + VMware, especially IDCF Cloud
-- Only tested in MacOS Sierra
+- Only tested in macOS Sierra
 
 ## Prequistes
 - VMware Fusion
 - [drscream/packer-post-processor-ovftool](https://github.com/drscream/packer-post-processor-ovftool) post-processor plugin
     - `ovftool`
 - [exoscale/cs](https://github.com/exoscale/cs) cli
+
+---
+
+- [RancherOS on IDCFクラウド\(CloudStack\)で格安Rancherクラスタを組みたい \- Qiita](http://qiita.com/2k0ri/items/028efaa13f3392414958)
+
--- upload.sh
+++ upload.sh
@@ -4,9 +4,11 @@ version="0.7.1"
 [ -f rancheros.iso ] || curl -o rancheros.iso "https://releases.rancher.com/os/v${version}/rancheros.iso"
 # export ISO_MD5="$(curl -s https://releases.rancher.com/os/v${version}/iso-checksums.txt | awk '/^md5/{print $2}')"
 packer build packer.json
+
 gsutil -o GSUtil:parallel_composite_upload_threshold=150M cp packer__vmware-iso.ova gs://s.k2i.ooo/rancheros-${version}-idcfcloud.ova
 gsutil acl set public-read gs://s.k2i.ooo/rancheros-${version}-idcfcloud.ova
 sleep 60
+
 date="$(date +%FT%T)"
 # cloudstack-api registerTemplate --name "RancherOS ${version} ${date}" --displaytext RancherOS --format OVA --hypervisor VMWare --ostypeid c20faebd-4ada-11e4-bd06-005056812ba5 --passwordenabled false --zoneid 01738d49-2722-4788-891e-848536663c6e --url http://s.k2i.ooo/rancheros-${version}-idcfcloud.ova
 CLOUDSTACK_ENDPOINT="https://compute.jp-east.idcfcloud.com/client/api" cs --region jp-east registerTemplate name="RancherOS ${version} ${date}" displaytext=RancherOS format=OVA hypervisor=VMWare ostypeid="c20faebd-4ada-11e4-bd06-005056812ba5" passwordenabled=false zoneid="01738d49-2722-4788-891e-848536663c6e" url="http://s.k2i.ooo/rancheros-${version}-idcfcloud.ova"
